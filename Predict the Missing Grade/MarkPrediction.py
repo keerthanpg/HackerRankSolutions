4e400381e002c6aa6eb2296286c0fc493a6243f7
keerthanpg <keerthanpg@gmail.com>
2016-05-27 00:41:04 +0530

    Debugged

--- Predict the Missing Grade/MarkPrediction.py
+++ Predict the Missing Grade/MarkPrediction.py
@@ -13,22 +13,32 @@ for i in range(N_train):
     for [k, v] in train_sample.iteritems():
         keys.append(k)
     if 'Accountancy' in keys:
-        X_acco.append([v for [k,v] in train_sample.iteritems() if k!='Serial' & k!='Mathematics'])
-        Y_acco.append(train_sample('Mathematics')
+        X_acco.append([v for [k,v] in train_sample.iteritems() if (k!='serial') & (k!='Mathematics')])
+        Y_acco.append(train_sample['Mathematics'])
     elif 'Biology' in keys:
-        X_bio.append([v for [k,v] in train_sample.iteritems() if k!='Serial' & k!='Mathematics'])
-        Y_bio.append(train_sample('Mathematics')
-    elif 'Computer Science' in keys:
-        X_cs.append([v for [k,v] in train_sample.iteritems() if k!='Serial' & k!='Mathematics'])
-        Y_cs.append(train_sample('Mathematics')
+        X_bio.append([v for [k,v] in train_sample.iteritems() if (k!='serial') & (k!='Mathematics')])
+        Y_bio.append(train_sample ['Mathematics'])
+    elif 'ComputerScience' in keys:
+        X_cs.append([v for [k,v] in train_sample.iteritems() if (k!='serial') & (k!='Mathematics')])
+        Y_cs.append(train_sample['Mathematics'])
     elif 'Economics' in keys:
-        X_eco.append([v for [k,v] in train_sample.iteritems() if k!='Serial' & k!='Mathematics'])
-        Y_eco.append(train_sample('Mathematics')
-    elif 'Physical Education' in keys:
-        X_pe.append([v for [k,v] in train_sample.iteritems() if k!='Serial' & k!='Mathematics'])
-        Y_pe.append(train_sample('Mathematics')                    
+        X_eco.append([v for [k,v] in train_sample.iteritems() if (k!='serial') & (k!='Mathematics')])
+        Y_eco.append(train_sample['Mathematics'])
+    elif 'PhysicalEducation' in keys:
+        X_pe.append([v for [k,v] in train_sample.iteritems() if (k!='serial') & (k!='Mathematics')])
+        Y_pe.append(train_sample['Mathematics'] )                   
         
 for a, b, c, d, e, f, g, h, i, j in zip(X_acco, Y_acco, X_bio, Y_bio, X_cs, Y_cs, X_eco, Y_eco, X_pe, Y_pe):
     print a, b, c, d, e, f, g, h, i, j
 
-#print X_acco[1], X_bio[1], X_cs[1])    
+print len(X_eco)
+print len(X_acco)
+print len(X_bio)
+print len(X_cs)
+print len(X_pe)
+
+print len(Y_eco)
+print len(Y_acco)
+print len(Y_bio)
+print len(Y_cs)
+print len(Y_pe)
